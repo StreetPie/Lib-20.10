3103b89c599e3ef245b3e5f7b9e01efbac7dc5ec
OparinDG <OparinDG@310D-1>
2023-10-20 15:24:43 +0700

    Добавьте файлы проекта.

---
+++ Lib/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Xml.Linq;
+using System.Text;
+using USQLCSLibrary;
+namespace LibraryConsoleApp
+{
+    class Program
+    {
+        static Book library = new Book();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ShowMenu();
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddBook();
+                        break;
+                    case 2:
+                        AddReader();
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.WriteLine("Неверный номер.");
+                        break;
+                }
+            }
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("1. Добавить книгу");
+            Console.WriteLine("2. Добавить читателя");
+            Console.WriteLine("3");
+            Console.Write("Выберите действие: ");
+        }
+
+        static void AddBook()
+        {
+            Console.Write("Введите артикул: ");
+            string articul = Console.ReadLine();
+
+            Console.Write("Введите автора: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Введите название: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Введите год издания: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество экземпляров: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            Book book = new Book
+            {
+                Articul = articul,
+                Author = author,
+                Title = title,
+                Year = year,
+                Quantity = quantity
+            };
+            
+            Library.AddBook(Book book);
+            Console.WriteLine("Книга успешно добавлена.");
+        }
+
+        static void AddReader()
+        {
+            Console.Write("Введите ФИО читателя: ");
+            string fullName = Console.ReadLine();
+
+            Console.Write("Введите номер группы: ");
+            string groupNumber = Console.ReadLine();
+        }
+    }
+}
+
+
+
+       
+
+   
+
+
+ 
---
+++ USQLCSLibrary/library.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using USQLCSLibrary;
+
+namespace USQLCSLibrary
+{
+    public class Book
+    {
+        public string Articul { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Reader
+    {
+        public string FullName { get; set; }
+        public string GroupNumber { get; set; }
+        public List<Book> Books { get; set; }
+
+        public Reader()
+        {
+            Books = new List<Book>();
+        }
+    }
+    public class Library
+    {
+        public List<Book> Books { get; set; }
+        public List<Reader> Readers { get; set; }
+
+        public Library()
+        {
+            Books = new List<Book>();
+            Readers = new List<Reader>();
+        }
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+        public void AddReader()
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
